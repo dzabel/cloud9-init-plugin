aa8cc51b4c946200d21126bed84d17f6fd246cf4
Daniel Zabel <daniel.zabel@coremedia.com>
2018-11-09 12:28:17 +0100

    add user handling

--- install.sh
+++ install.sh
@@ -13,52 +13,124 @@ end=$'\e[0m'
 
 
 ##### Path and files
-logfile="install.log"
+envDir=$(realpath ~/environment)
+envDir=$(echo "$envDir/cloud")
 
-installDir=`realpath ~/environment`
-installDir=`echo "$installDir/cloud"`
+copyDir="/private/cloud"
 
 #cmd msg
 chkStatus() {
-  if [ $# -eq 2 ]; then
+  if [[ $# -eq 2 ]]; then
     msg=$2
   else
     msg=$1
   fi
   outputmsg=`$1 `
-  if [ $? -eq 0 ]; then
-  printf "$msg        [${grn} OK ${end}]\n"
-else
-  printf "$msg        [${red} Failed ${end}]\n"
-  exit 1
+  if [[ $? -eq 0 ]]; then
+    printf "$msg        [${grn} OK ${end}]\n"
+  else
+    printf "$msg        [${red} Failed ${end}]\n"
+    exit 1
   fi
+}
+
+createUser() {
+  local user=$1
+
+  chkStatus "
+  sudo adduser \
+     --system \
+     --shell /bin/bash \
+     --gecos 'User for managing cmoc stack' \
+     --group \
+     --disabled-password \
+     --home /home/${user} \
+     ${user}" "create user ${user}"
+
+echo "${red}choose your gpg password (remember and keep safe): ${end}"
+read GPG_PWD
+
+chkStatus "sudo su - ${user} \"cat >foo <<EOF
+     %echo Generating a OpenPGP key
+     Key-Type: default
+     Key-Length: default
+     Name-Real: ${GITHUB_USER}
+     Name-Comment: cmoc-developer
+     Name-Email: ${GITHUB_EMAIL}
+     Expire-Date: 0
+     Passphrase: ${GPG_PWD}
+     %commit
+     %echo done
+EOF\"" "prepare gpg key generation"
+chkStatus "sudo su - ${user} \"gpg --batch --generate-key foo\"" "generate gpg key"
+
+sudo su - ${user} "cat >  ~/.netrc <<EOF
+machine github.com
+login ${GITHUB_USER}
+password ${GITHUB_TOKEN}
+protocol https
+
+machine gist.github.com
+login ${GITHUB_USER}
+password ${GITHUB_TOKEN}
+protocol https
+EOF"
+
+chkStatus "sudo su - ${user} \"gpg -e -r ${user} ~/.netrc\"" "encrypt temporary ~/.netrc with your key"
+chkStatus "sudo su - ${user} \"rm -f ~/.netrc\"" "delete temp ~/.netrc"
+
+sudo su - ${user} "curl --silent -o ~/.local/bin/git-credential-netrc https://raw.githubusercontent.com/git/git/master/contrib/credential/netrc/git-credential-netrc"
+
+chkStatus "sudo su - ${user} \"git config --local user.name ${GITHUB_USER}\"" "set git user"
+chkStatus "sudo su - ${user} \"git config --local user.email ${GITHUB_EMAIL}\"" "set git email"
+
+sudo su - ${user} "cat >~/.profile<<\EOF
+# Invoke GnuPG-Agent the first time we login.
+# Does ~/.gpg-agent-info exist and points to gpg-agent process accepting signals?
+if test -f $HOME/.gpg-agent-info && \
+    kill -0 $(cut -d: -f 2 $HOME/.gpg-agent-info) 2>/dev/null; then
+    GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info | cut -c 16-`
+else
+    # No, gpg-agent not available; start gpg-agent
+    eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
+fi
+export GPG_TTY=`tty`
+export GPG_AGENT_INFO
+EOF"
+
+chkStatus "sudo su - ${user} \"ln -sf ${copyDir} ~/cloud\"" "symlink ~/cloud"
+
 }
 
 ####### Installation Start
 
-chkStatus "mkdir -p $installDir"
+chkStatus "mkdir -p $copyDir;ln -s $copyDir $envDir" "create directories"
 
-######## Update System
-chkStatus "sudo yum -y update"
+######## Prepare System
+chkStatus "sudo yum -y update" "${blu} Updating OS ${end}"
+chkStatus "sudo yum -y install git jq" "installing tools"
 
-######## Installing GIT
-chkStatus "sudo yum -y install git"
+chkStatus "sudo groupadd cmocdev" "create cmocdev group"
 
 ######### Configure GIT
-echo "Input your github user:"
+echo "${blu} Input your github user ${end} ${blu} (becomes your \"su -\" user for daily work): ${end}"
 read GITHUB_USER
 export GITHUB_USER=${GITHUB_USER}
 
-echo "Input your github access token:"
-read GITHUB_TOKEN
+echo "${blu} Input your github email: ${end}"
+read GITHUB_EMAIL
+export GITHUB_EMAIL=${GITHUB_EMAIL}
+
+echo "${red} Input your github access token: ${red}"
+read -s GITHUB_TOKEN
 export GITHUB_TOKEN=${GITHUB_TOKEN}
 
-chkStatus "git config --global credential.helper store" "git credentials store"
-chkStatus "git config --global user.name ${GITHUB_USER}" "set git user"
+chkStatus "git config --global credential.helper \"netrc -f ~/.netrc.gpg -v\"" "configure git credentials store"
+
 cat >  ~/.git-credentials <<EOF
 https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com
 EOF
-cd $installDir
+cd $copyDir
 if [ -d "cloud-infrastructure-config" ]; then
   chkStatus "cd cloud-infrastructure-config && git pull -q && cd .." "update master-config"
 else
@@ -71,3 +143,4 @@ else
   chkStatus "git clone -q https://github.com/CoreMedia/cloud-infrastructure cloud-infrastructure" "clone cloud-infrastructure"
 fi
 
+createUser $GITHUB_USER
