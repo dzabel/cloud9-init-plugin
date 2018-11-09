ce2429a951b07142058d9dd551dda61e942a19fe
Daniel Zabel <daniel.zabel@coremedia.com>
2018-11-09 16:15:03 +0100

    revert multi user feature, do single user install

--- install.sh
+++ install.sh
@@ -16,8 +16,6 @@ end=$'\e[0m'
 envDir=$(realpath ~/environment)
 envDir=$(echo "$envDir/cloud")
 
-copyDir="/private/cloud"
-
 #cmd msg
 chkStatus() {
   if [[ $# -eq 2 ]]; then
@@ -34,83 +32,13 @@ chkStatus() {
   fi
 }
 
-createUser() {
-  local user=$1
-
-  chkStatus "
-  sudo adduser \
-     --system \
-     --shell /bin/bash \
-     --gecos 'User for managing cmoc stack' \
-     --group \
-     --disabled-password \
-     --home /home/${user} \
-     ${user}" "create user ${user}"
-
-echo "${red}choose your gpg password (remember and keep safe): ${end}"
-read GPG_PWD
-
-chkStatus "sudo su - ${user} \"cat >foo <<EOF
-     %echo Generating a OpenPGP key
-     Key-Type: default
-     Key-Length: default
-     Name-Real: ${GITHUB_USER}
-     Name-Comment: cmoc-developer
-     Name-Email: ${GITHUB_EMAIL}
-     Expire-Date: 0
-     Passphrase: ${GPG_PWD}
-     %commit
-     %echo done
-EOF\"" "prepare gpg key generation"
-chkStatus "sudo su - ${user} \"gpg --batch --generate-key foo\"" "generate gpg key"
-
-sudo su - ${user} "cat >  ~/.netrc <<EOF
-machine github.com
-login ${GITHUB_USER}
-password ${GITHUB_TOKEN}
-protocol https
-
-machine gist.github.com
-login ${GITHUB_USER}
-password ${GITHUB_TOKEN}
-protocol https
-EOF"
-
-chkStatus "sudo su - ${user} \"gpg -e -r ${user} ~/.netrc\"" "encrypt temporary ~/.netrc with your key"
-chkStatus "sudo su - ${user} \"rm -f ~/.netrc\"" "delete temp ~/.netrc"
-
-sudo su - ${user} "curl --silent -o ~/.local/bin/git-credential-netrc https://raw.githubusercontent.com/git/git/master/contrib/credential/netrc/git-credential-netrc"
-
-chkStatus "sudo su - ${user} \"git config --local user.name ${GITHUB_USER}\"" "set git user"
-chkStatus "sudo su - ${user} \"git config --local user.email ${GITHUB_EMAIL}\"" "set git email"
-
-sudo su - ${user} "cat >~/.profile<<\EOF
-# Invoke GnuPG-Agent the first time we login.
-# Does ~/.gpg-agent-info exist and points to gpg-agent process accepting signals?
-if test -f $HOME/.gpg-agent-info && \
-    kill -0 $(cut -d: -f 2 $HOME/.gpg-agent-info) 2>/dev/null; then
-    GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info | cut -c 16-`
-else
-    # No, gpg-agent not available; start gpg-agent
-    eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
-fi
-export GPG_TTY=`tty`
-export GPG_AGENT_INFO
-EOF"
-
-chkStatus "sudo su - ${user} \"ln -sf ${copyDir} ~/cloud\"" "symlink ~/cloud"
-
-}
-
 ####### Installation Start
 
-chkStatus "mkdir -p $copyDir;ln -s $copyDir $envDir" "create directories"
+chkStatus "mkdir -p $envDir" "create directories"
 
 ######## Prepare System
 chkStatus "sudo yum -y update" "${blu} Updating OS ${end}"
 chkStatus "sudo yum -y install git jq" "installing tools"
-
-chkStatus "sudo groupadd cmocdev" "create cmocdev group"
 
 ######### Configure GIT
 echo "${blu} Input your github user ${end} ${blu} (becomes your \"su -\" user for daily work): ${end}"
@@ -127,10 +55,59 @@ export GITHUB_TOKEN=${GITHUB_TOKEN}
 
 chkStatus "git config --global credential.helper \"netrc -f ~/.netrc.gpg -v\"" "configure git credentials store"
 
-cat >  ~/.git-credentials <<EOF
-https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com
+echo "${red}choose your gpg password (remember and keep safe): ${end}"
+read GPG_PWD
+
+chkStatus "cat >foo <<EOF
+     %echo Generating a OpenPGP key
+     Key-Type: default
+     Key-Length: default
+     Name-Real: ${GITHUB_USER}
+     Name-Comment: cmoc-developer
+     Name-Email: ${GITHUB_EMAIL}
+     Expire-Date: 0
+     Passphrase: ${GPG_PWD}
+     %commit
+     %echo done
+EOF" "prepare gpg key generation"
+
+chkStatus "gpg --batch --generate-key foo" "generate gpg key"
+
+cat >  ~/.netrc <<EOF
+machine github.com
+login ${GITHUB_USER}
+password ${GITHUB_TOKEN}
+protocol https
+
+machine gist.github.com
+login ${GITHUB_USER}
+password ${GITHUB_TOKEN}
+protocol https
 EOF
-cd $copyDir
+
+chkStatus "gpg -e -r ${user} ~/.netrc" "encrypt temporary ~/.netrc with your key"
+chkStatus "rm -f ~/.netrc" "delete temp ~/.netrc"
+
+curl --silent -o ~/.local/bin/git-credential-netrc https://raw.githubusercontent.com/git/git/master/contrib/credential/netrc/git-credential-netrc
+
+chkStatus "git config --local user.name ${GITHUB_USER}" "set git user"
+chkStatus "git config --local user.email ${GITHUB_EMAIL}" "set git email"
+
+cat >~/.profile<<\EOF
+# Invoke GnuPG-Agent the first time we login.
+# Does ~/.gpg-agent-info exist and points to gpg-agent process accepting signals?
+if test -f $HOME/.gpg-agent-info && \
+    kill -0 $(cut -d: -f 2 $HOME/.gpg-agent-info) 2>/dev/null; then
+    GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info | cut -c 16-`
+else
+    # No, gpg-agent not available; start gpg-agent
+    eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
+fi
+export GPG_TTY=`tty`
+export GPG_AGENT_INFO
+EOF
+
+cd $envDir
 if [ -d "cloud-infrastructure-config" ]; then
   chkStatus "cd cloud-infrastructure-config && git pull -q && cd .." "update master-config"
 else
@@ -142,5 +119,3 @@ if [ -d "cloud-infrastructure" ]; then
 else
   chkStatus "git clone -q https://github.com/CoreMedia/cloud-infrastructure cloud-infrastructure" "clone cloud-infrastructure"
 fi
-
-createUser $GITHUB_USER
