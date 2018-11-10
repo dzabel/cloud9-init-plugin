9cfb0bf4bcb89d7e006b4393df5607b201143b81
Daniel Zabel <daniel.zabel@coremedia.com>
2018-11-10 01:22:17 +0100

    no use of gpg, use simple way for git

--- install.sh
+++ install.sh
@@ -53,26 +53,6 @@ echo "${red} Input your github access token: ${red}"
 read GITHUB_TOKEN
 export GITHUB_TOKEN=${GITHUB_TOKEN}
 
-chkStatus "git config --local credential.helper 'netrc -f ~/.netrc.gpg -v'" "configure git credentials store"
-
-echo "${red}choose your gpg password (remember and keep safe): ${end}"
-read GPG_PWD
-
-chkStatus "cat >foo <<EOF
-     %echo Generating a OpenPGP key
-     Key-Type: default
-     Key-Length: default
-     Name-Real: ${GITHUB_USER}
-     Name-Comment: cmoc-developer
-     Name-Email: ${GITHUB_EMAIL}
-     Expire-Date: 0
-     Passphrase: ${GPG_PWD}
-     %commit
-     %echo done
-EOF" "prepare gpg key generation"
-
-chkStatus "gpg --batch --generate-key foo" "generate gpg key"
-
 cat >  ~/.netrc <<EOF
 machine github.com
 login ${GITHUB_USER}
@@ -85,27 +65,8 @@ password ${GITHUB_TOKEN}
 protocol https
 EOF
 
-chkStatus "gpg -e -r ${user} ~/.netrc" "encrypt temporary ~/.netrc with your key"
-chkStatus "rm -f ~/.netrc" "delete temp ~/.netrc"
-
-curl --silent -o ~/.local/bin/git-credential-netrc https://raw.githubusercontent.com/git/git/master/contrib/credential/netrc/git-credential-netrc
-
-chkStatus "git config --local user.name ${GITHUB_USER}" "set git user"
-chkStatus "git config --local user.email ${GITHUB_EMAIL}" "set git email"
-
-cat >~/.profile<<\EOF
-# Invoke GnuPG-Agent the first time we login.
-# Does ~/.gpg-agent-info exist and points to gpg-agent process accepting signals?
-if test -f $HOME/.gpg-agent-info && \
-    kill -0 $(cut -d: -f 2 $HOME/.gpg-agent-info) 2>/dev/null; then
-    GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info | cut -c 16-`
-else
-    # No, gpg-agent not available; start gpg-agent
-    eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
-fi
-export GPG_TTY=`tty`
-export GPG_AGENT_INFO
-EOF
+chkStatus "git config --global user.name ${GITHUB_USER}" "set git user"
+chkStatus "git config --global user.email ${GITHUB_EMAIL}" "set git email"
 
 cd $envDir
 if [ -d "cloud-infrastructure-config" ]; then
